3c3b74edc8e0ceb8715e3a5bb3af95c6ab58208d
BobcatsII <ln286577399@gmail.com>
2018-09-17 17:46:27 +0800

    Update iptables_one_way.sh

--- iptables/iptables_one_way.sh
+++ iptables/iptables_one_way.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+#需求：实现 192.168.10.*  网段不能访问 100/200 网段内的所有服务器 ，而 192.168.100/200.* 网段内的服务器可以访问 192.168.10.31 服务器
+
 num=`/sbin/ifconfig -a|grep inet|grep -v 127.0.0.1|grep -v inet6|awk '{print $2}'|tr -d "addr:"|awk -F'.' '{print $3}'`
 
 if [ $num == '100'] || [ $num == '200' ]
