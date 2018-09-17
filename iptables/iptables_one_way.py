cfeaca219316fad17839a7a08828a0c16a25687c
BobcatsII <ln286577399@gmail.com>
2018-09-17 17:46:05 +0800

    Update iptables_one_way.py

--- iptables/iptables_one_way.py
+++ iptables/iptables_one_way.py
@@ -4,6 +4,10 @@
 import iptc
 import commands
 
+"""
+    需求：实现 192.168.10.*  网段不能访问 100/200 网段内的所有服务器 ，而 192.168.100/200.* 网段内的服务器可以访问 192.168.10.31 服务器
+"""
+
 num = commands.getoutput("/sbin/ifconfig -a|grep inet|grep -v 127.0.0.1|grep -v inet6|awk '{print $2}'|tr -d 'addr:'|awk -F'.' '{print $3}'")
 
 if num == '100' or num == '200':
